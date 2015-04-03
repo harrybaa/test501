cc1099c2f4e3982bf186a068d050d63e2566011e
xelloss00x <956127823@qq.com>
2015-04-03 16:30:07 +0800

    增加了回复自动获取用户名的功能

--- Lbbs5.0/src/com/bbs/action/bbsAnswer/AddBbsAnswerAction.java
+++ Lbbs5.0/src/com/bbs/action/bbsAnswer/AddBbsAnswerAction.java
@@ -3,6 +3,7 @@ package com.bbs.action.bbsAnswer;
 import java.util.Map;
 
 import com.bbs.model.BbsAnswer;
+import com.bbs.model.User;
 import com.bbs.service.BbsAnswerService;
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionContext;
@@ -34,6 +35,10 @@ public class AddBbsAnswerAction implements Action {
 	    Map session = actionContext.getSession();
 	    bbsAnswer.setRootId((Integer) session.get("rootId"));
 	    System.out.println(bbsAnswer.getContent());
+	    Map map = (Map) ActionContext.getContext().get("session");
+	    User user = (User) map.get("user");
+	    System.out.println(user.getName());
+	    bbsAnswer.setSender(user.getName());
 		bbsAnswerService.addBbsAnswer(bbsAnswer);
 		return SUCCESS;
 	}
